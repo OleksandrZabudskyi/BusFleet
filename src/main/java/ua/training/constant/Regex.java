a1632fbc804c3d75274e2517b44e2bba0397d37e
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-05-14 12:28:21 +0300

    Authorization refactoring

--- src/main/java/ua/training/constant/Regex.java
+++ src/main/java/ua/training/constant/Regex.java
@@ -3,4 +3,5 @@ package ua.training.constant;
 public interface Regex {
     String REGEX_EMAIL = "^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$";
     String PHONE = "^\\+\\d{2}\\d{3}\\d{3}-\\d{2}-\\d{2}$";
+    String URL = "(.*\\/app\\/)|(bus-fleet\\/)|(admin\\/)|(driver\\/)";
 }
