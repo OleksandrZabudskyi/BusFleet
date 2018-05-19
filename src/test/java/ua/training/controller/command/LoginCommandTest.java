0f2349978cbb794a5fd493ae2dbeac71503a8063
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-05-19 16:53:10 +0300

    add db script, update README.md

--- src/test/java/ua/training/controller/command/LoginCommandTest.java
+++ src/test/java/ua/training/controller/command/LoginCommandTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import ua.training.model.service.EmployeeService;
+import ua.training.model.service.SecurityService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,15 +14,17 @@ public class LoginCommandTest {
     @Mock
     private EmployeeService employeeService;
     @Mock
+    private SecurityService securityService;
+    @Mock
     private HttpServletRequest request;
     @Mock
     private HttpServletResponse response;
     private LoginCommand loginCommand;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
-        loginCommand = new LoginCommand(employeeService);
+        loginCommand = new LoginCommand(employeeService, securityService);
     }
 
     @Test
